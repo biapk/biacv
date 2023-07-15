a383173c20cb30b9cc4cf782dabe465408d10b83
Alipk1 <hmd850@gmail.com>
2023-07-15 11:59:06 +0500

    first commit

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -1,6 +1,6 @@
 function initMap() {
     // Latitude and Longitude
-    var myLatLng = {lat: -6.207690, lng: 106.985270};
+    var myLatLng = {lat: 24.815836812162253, lng:67.11011163681702};
 
     var map = new google.maps.Map(document.getElementById('google-maps'), {
         zoom: 17,
@@ -10,6 +10,7 @@ function initMap() {
     var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
-        title: 'South Jakarta, INA' // Title Location
+        title: 'Korangi, KHI' // Title Location
     });
 }
+24.815836812162253, 67.11011163681702
